85365105641e5968f17488c9d6a473e37927a2d1
galubezzz <galubezzz@gmail.com>
2019-02-13 00:08:59 -0800

    added main logic for modal

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Modal from './components/UI/Modal/Modal'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+        this.state.modal = false;
+    }
+
+    showModal = () => {
+        this.setState({modal: true})
+    }
+
+    closeModal = () => {
+        this.setState({modal: false})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Modal title="Заголовок модального окна"
+                       show={this.state.modal}
+                       close={this.closeModal}>Text</Modal>
+                <button onClick={this.showModal}>Modal</button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,13 +4,38 @@ import Backdrop from "../Backdrop/Backdrop";
 
 
 const Modal = props => (
+
     <div>
-        <Backdrop show={props.show} cancel={props.cancel}/>
-        <div className={"Modal" + (props.show ? ' Modal-show' : '')}>
-            {props.children}
+        <Backdrop show={props.show} cancel={props.close}/>
+        {/*<div className={"Modal" + (props.show ? ' Modal-show' : '')}>*/}
+            {/*{props.children}*/}
+        {/*</div>*/}
+        <div className={props.show ? "modal fade show" : "modal fade"}
+             style={{ display: props.show ? "block" : "none"}}
+             tabIndex="-1" role="dialog">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{props.title}</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={props.close}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <p>Modal body text goes here.</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={props.close}>Close</button>
+                        <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
+
 );
 
 
 export default Modal;
+{}
